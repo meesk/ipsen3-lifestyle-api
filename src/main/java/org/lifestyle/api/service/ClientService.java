ae947e7013998deac3dbbb74a23505d2d1c36b2d
s1103941 <s1103941@student.hsleiden.nl>
2017-12-12 10:47:53 +0100

    clientservice is functional

---
+++ src/main/java/org/lifestyle/api/service/ClientService.java
@@ -0,0 +1,40 @@
+package org.lifestyle.api.service;
+
+import java.util.Collection;
+import org.lifestyle.api.model.Client;
+import org.lifestyle.api.persistence.ClientDAO;
+
+/**
+ *
+ * @author abdoul
+ */
+public class ClientService extends BaseService<Client> {
+    
+    private final ClientDAO dao;
+    
+    public ClientService(){
+        this.dao = new ClientDAO();
+    }
+    
+    public Collection<Client> getAllClients(){
+        return dao.getClient();
+    }
+    
+    public void addClient(Client client){
+        dao.addClient(client);
+    }
+    
+    public void getClient(int id){
+        dao.get(id);
+    }
+    
+    public void deleteClient(int id){
+        dao.removeClient(id);
+    }
+    
+    public void updateClient(int id, Client client){
+      dao.updateClient(id, client);
+    }
+}
+
+
